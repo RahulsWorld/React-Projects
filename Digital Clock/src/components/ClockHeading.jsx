baf627132a79445ecea8ceccbb185894b0ffabc3
Rahul Rathour <rahulrathour7068@gmail.com>
2024-11-21 21:54:23 +0530

    Add files via upload

---
+++ Digital Clock/src/components/ClockHeading.jsx
@@ -0,0 +1,4 @@
+function ClockHeading() {
+  return <h1>Digital Bharat Clock</h1>;
+}
+export default ClockHeading;
---
+++ Digital Clock/src/components/ClockSlogan.jsx
@@ -0,0 +1,4 @@
+function ClockSlogan(){
+return <p>This is the clock  which shows the time of Bharat at all time.</p>
+};
+export default ClockSlogan;
