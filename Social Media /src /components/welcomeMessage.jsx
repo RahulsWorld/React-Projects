e4a70543f5a0d3cca8ecba83c933048f73b8f321
Rahul Rathour <rahulrathour7068@gmail.com>
2024-11-30 23:07:36 +0530

    Delete Social Media  directory

--- Social Media /src /components/welcomeMessage.jsx
+++
@@ -1,8 +0,0 @@
-const WelcomeMessage = () => {
-  return (
-    <h2 className="Message">
-      There are no Post . Add some Posts By 'Create Post' Option in SideBar
-    </h2>
-  );
-};
-export default WelcomeMessage;
