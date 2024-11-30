c85595970a08aa1c7c3edacf9de937988161dcf4
Rahul Rathour <rahulrathour7068@gmail.com>
2024-11-30 23:05:07 +0530

    Add files via upload

---
+++ Social Media /src /components/welcomeMessage.jsx
@@ -0,0 +1,8 @@
+const WelcomeMessage = () => {
+  return (
+    <h2 className="Message">
+      There are no Post . Add some Posts By 'Create Post' Option in SideBar
+    </h2>
+  );
+};
+export default WelcomeMessage;
