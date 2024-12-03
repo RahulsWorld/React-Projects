500559cfdabb271b92df2fe4c43781b3daab8330
Rahul Rathour <rahulrathour7068@gmail.com>
2024-12-03 22:05:19 +0530

    Update ClockTime.jsx

--- Digital Clock/src/components/ClockTime.jsx
+++ Digital Clock/src/components/ClockTime.jsx
@@ -1,12 +1,22 @@
+import { useEffect, useState } from "react";
+
 function ClockTime() {
-  let time = new Date();
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
   return (
-    <form>
+    <>
       This is the current time:{time.toLocaleDateString()} -{" "}
       {time.toLocaleTimeString()}
-      <button>Refresh</button>
-    </form>
+    </>
   );
 }
 export default ClockTime;
